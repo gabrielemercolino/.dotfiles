0fbf65eb381eaf9f45ef5f2a29efc7cbb0c1f19e
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-01-13 12:19:56 +0100

    Rollback and cleaning

--- hyprland.nix
+++ hyprland.nix
@@ -4,93 +4,84 @@
 	wayland.windowManager.hyprland = {
 		enable = true;
 		xwayland.enable = true;
-		settings = {
-			input {
-			    kb_layout = "it";
-			    kb_variant = "";
-			    kb_model = "";
-			    kb_options = "";
-			    kb_rules = "";
-			
-			    follow_mouse = 1;
-			
-			    touchpad {
-			        natural_scroll = "no";
-			    };
-			
-			    sensitivity = 0;
-			};
-
-			general {
-			    gaps_in = 5;
-			    gaps_out = 20;
-			    border_size = 2;
-			    col.active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-			    col.inactive_border = "rgba(595959aa)";
-			    layout = "dwindle";
-			    allow_tearing = false;
-			};
-
-			decoration {
-			    rounding = 10;
-			    
-			    blur {
-			        enabled = true;
-			        size = 3;
-			        passes = 1;
-			    };
-			
-			    drop_shadow = yes;
-			    shadow_range = 4;
-			    shadow_render_power = 3;
-			    col.shadow = "rgba(1a1a1aee)";
-			};
-
-			animations {
-			    enabled = "yes";
-			    bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-			
-			    animation = "windows, 1, 7, myBezier";
-			    animation = "windowsOut, 1, 7, default, popin 80%";
-			    animation = "border, 1, 10, default";
-			    animation = "borderangle, 1, 8, default";
-			    animation = "fade, 1, 7, default";
-			    animation = "workspaces, 1, 6, default";
-			};
-
-			dwindle {
-			    pseudotile = "yes";
-			    preserve_split = "yes";
-			};
-			
-			master {
-			    new_is_master = true;
-			};
-			
-			gestures {
-			    workspace_swipe = "on";
-			};
-			
-			misc {
-			    force_default_wallpaper = 0;
-			};
-		};
-		extraConfig = ''		
+		settings = {};
+		extraConfig = ''			
 			# See https://wiki.hyprland.org/Configuring/Monitors/
 			monitor=,highres,auto,1
-
+			
 			# Some default env vars.
 			env = XCURSOR_SIZE,24
-
-			# Example per-device config
+			
+			input {
+			    kb_layout = it
+			    kb_variant =
+			    kb_model =
+			    kb_options =
+			    kb_rules =
+			    follow_mouse = 1
+			    touchpad {
+			        natural_scroll = no
+			    }
+			    sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+			}
+			
+			general {			
+			    gaps_in = 5
+			    gaps_out = 20
+			    border_size = 2
+			    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+			    col.inactive_border = rgba(595959aa)
+			    layout = dwindle
+			    allow_tearing = false
+			}
+			
+			decoration {
+			    rounding = 10
+			    blur {
+			        enabled = true
+			        size = 3
+			        passes = 1
+			    }
+			    drop_shadow = yes
+			    shadow_range = 4
+			    shadow_render_power = 3
+			    col.shadow = rgba(1a1a1aee)
+			}
+			
+			animations {
+			    enabled = yes
+			    bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+			    animation = windows, 1, 7, myBezier
+			    animation = windowsOut, 1, 7, default, popin 80%
+			    animation = border, 1, 10, default
+			    animation = borderangle, 1, 8, default
+			    animation = fade, 1, 7, default
+			    animation = workspaces, 1, 6, default
+			}
+			
+			dwindle {
+			    pseudotile = yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+			    preserve_split = yes # you probably want this
+			}
+			
+			master {
+			    new_is_master = true
+			}
+			
+			gestures {
+			    workspace_swipe = on
+			}
+			
+			misc {
+			    force_default_wallpaper = 0
+			}
+			
 			device:epic-mouse-v1 {
 			    sensitivity = -0.5
-			}
-				
-			# See https://wiki.hyprland.org/Configuring/Keywords/ for more
+			}			
+			
 			$mainMod = SUPER
 			
-			# Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
 			bind = $mainMod, RETURN, exec, kitty
 			bind = $mainMod, Q, killactive, 
 			bind = $mainMod, M, exit, 
