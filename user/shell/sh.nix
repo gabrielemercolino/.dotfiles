e53eb4b6784bdaebe0709ffc688887e3c5503ea0
gabrielemercolino <gmercolino2003@gmail.com>
2024-03-13 22:18:30 +0100

    refactor: fixed aliases and indentation

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -1,28 +1,29 @@
-{ config, pkgs, ... }:
-let
-	myShellAliases =  {
-    ls = "eza --icons";
-		ll = "eza -l --icons";
-		la = "eza -la --icons";
-  	};
-in
-
 {
-	programs.bash = {
-		enable = true;
-		shellAliases = myShellAliases;
-	};
-
-	programs.zsh = {
-		enable = true; 
- 		shellAliases = myShellAliases;
-		oh-my-zsh = {
-			enable = true;
-			theme = "robbyrussell";
-			plugins = ["git" "sudo"];
-		};
-		syntaxHighlighting.enable = true;
+  config,
+  pkgs,
+  ...
+}: let
+  myShellAliases = {
+    ls = "eza --icons";
+    ll = "eza -l --icons";
+    la = "eza -la --icons";
   };
-  
+in {
+  programs.bash = {
+    enable = true;
+    shellAliases = myShellAliases;
+  };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = myShellAliases;
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = ["git" "sudo"];
+    };
+    syntaxHighlighting.enable = true;
+  };
+
   home.packages = [pkgs.eza];
 }
