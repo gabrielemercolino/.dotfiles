75379519235b2afa06e86d9b4c758bd186b9c086
gabrielemercolino <gmercolino2003@gmail.com>
2024-03-19 22:26:20 +0100

    refactor: changed vscodium config following docs

--- user/apps/editors/vscodium.nix
+++ user/apps/editors/vscodium.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
 
 {
-	home.packages = with pkgs; [
-		vscodium
-	];
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+
+    extensions = with pkgs.vscode-extensions; [
+      wix.vscode-import-cost
+      jnoortheen.nix-ide
+    ];
+  };
 }
