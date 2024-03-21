3173f6fb62dc556d7aa8359fd34228d6b87571af
gabrielemercolino <gmercolino2003@gmail.com>
2024-03-21 21:43:40 +0100

    refactor: filtered nerd fonts and added other ones

---
+++ system/fonts/default.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+	fonts.packages = with pkgs; [
+		(nerdfonts.override { fonts = [ "Inconsolata" "DejaVuSansMono" "JetBrainsMono" "Meslo" ]; })
+    font-awesome
+    powerline
+	];
+}
