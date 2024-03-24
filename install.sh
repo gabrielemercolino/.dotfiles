fcd3390dd9828cb2367a9739cf5c780a43482d06
gabrielemercolino <gmercolino2003@gmail.com>
2024-03-24 23:04:33 +0100

    refactor: cleaned up script

--- install.sh
+++ install.sh
@@ -1,13 +1,7 @@
-#!/bin/bash
-
-set -e
+#!/bin/sh
 
 sudo nixos-generate-config --show-hardware-config > ./system/hardware-configuration.nix
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
+sudo nixos-rebuild switch --flake ~/.dotfiles#system
 
-sudo nixos-rebuild switch --flake ~/.dotfiles/
-
-home-manager switch --flake ~/.dotfiles/
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.dotfiles#user
