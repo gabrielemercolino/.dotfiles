767d412c2f6e8cc11df4fbb0e4fbe865b69aa9a7
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-06-27 11:12:08 +0200

    feat: removed vscode and added zed editor

--- profiles/work/home.nix
+++ profiles/work/home.nix
@@ -4,14 +4,15 @@
   imports = [
 	../base/home.nix
   #../../user/apps/editors/sublime.nix
-  ../../user/apps/editors/vscodium.nix
+  #../../user/apps/editors/vscodium.nix
   ../../user/apps/editors/jetbrains.nix
   ../../user/apps/editors/nixvim.nix
+  ../../user/apps/editors/zed.nix
   ../../user/apps/utilities
-  ];  
+  ];
 
   home.sessionVariables = {
     EDITOR = "nvim";
   };
-  
+
 }
