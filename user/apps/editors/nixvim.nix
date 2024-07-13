06462aeb903c69302e4bf553f1fce6f507ce416e
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-07-13 21:50:14 +0200

    chore: removed old comments

--- user/apps/editors/nixvim.nix
+++ user/apps/editors/nixvim.nix
@@ -1,14 +1,7 @@
 { inputs, pkgs, userSettings, systemSettings, ... }:
 
-let
-nixvimScript = ''
-nix run github:gabrielemercolino/.nixvim
-'';
-
-in
 {
   home.packages = [
-    #(pkgs.writeScriptBin "nvim" nixvimScript)
     inputs.nixvim.packages.${systemSettings.system}.default
   ];
 }
