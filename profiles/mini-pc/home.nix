868c18efcff22b75704849e722f1b5b10d918578
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-07-22 14:59:32 +0200

    feat: added spotify with rice

--- profiles/mini-pc/home.nix
+++ profiles/mini-pc/home.nix
@@ -8,6 +8,9 @@
     ../../user/apps/editors/jetbrains.nix
     ../../user/apps/editors/zed.nix
 
+    # music
+    ../../user/apps/music/spicetify.nix
+
     # general utilities
     ../../user/apps/utilities
   ];
