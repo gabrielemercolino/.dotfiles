e64ca6977cfc2087946d65e9a08582f0038fe56b
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-08-21 16:40:01 +0200

    chore: removed gamescope session and simplifies protonge installation

--- system/gaming/steam.nix
+++ system/gaming/steam.nix
@@ -1,17 +1,7 @@
-{ pkgs, userSettings, ... }:
+{ pkgs, ... }:
 
 {
   programs.steam.enable = true;
 
-  programs.steam.gamescopeSession = {
-    enable = true;
-    env = {};
-    args = [];
-  };
-
-  environment.systemPackages = with pkgs; [ mangohud protonup ];
-
-  environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/" + userSettings.userName + "/.steam/root/compatibilitytools.d";
-  };
+  programs.steam.extraCompatPackages = [ pkgs.proton-ge-bin ];
 }
