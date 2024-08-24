ef1fecbba976c443ee696011398e031972535224
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-08-24 12:20:53 +0200

    fix: removed styles as stylix handles that

--- user/wm/hyprland/default.nix
+++ user/wm/hyprland/default.nix
@@ -121,8 +121,8 @@
       gaps_in = 5;
       gaps_out = 20;
       border_size = 2;
-      "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-      "col.inactive_border" = "rgba(595959aa)";
+      #"col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      #"col.inactive_border" = "rgba(595959aa)";
 
       layout = "dwindle";
 
@@ -144,7 +144,7 @@
       drop_shadow = "yes";
       shadow_range = 4;
       shadow_render_power = 3;
-      "col.shadow" = "rgba(1a1a1aee)";
+      #"col.shadow" = "rgba(1a1a1aee)";
     };
   
     dwindle = {
