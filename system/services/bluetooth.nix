f8faa9f1ffc4ac6657cc9a9c028fcb42ea99b739
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-08-25 11:50:19 +0200

    chore: fixed indentation and comment

--- system/services/bluetooth.nix
+++ system/services/bluetooth.nix
@@ -4,5 +4,5 @@
   hardware.bluetooth.enable = true;      # enables support for bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
-  services.blueman.enable = true; # cli to control bluetooth
+  services.blueman.enable = true;        # to control bluetooth
 }
