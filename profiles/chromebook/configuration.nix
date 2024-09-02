1f2208d872f39d76f4c541b41632aade8741f31d
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-09-02 22:08:34 +0200

    feat: using cachix for hyprland

--- profiles/chromebook/configuration.nix
+++ profiles/chromebook/configuration.nix
@@ -9,4 +9,10 @@
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # I'm not building hyprland on a Pentium 💀
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
