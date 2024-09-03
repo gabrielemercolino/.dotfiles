75a654c59441689c126a9298412d7e903bf4a177
Gabriele Mercolino <gmercolino2003@gmail.com>
2024-09-03 15:10:24 +0200

    feat: now actually cleaning home-manager generations

--- user/commands/gab/src/clean_command.sh
+++ user/commands/gab/src/clean_command.sh
@@ -1,7 +1,12 @@
 delete_old=${args[--delete-old]}
 
+TIMESPAN="30d"
+
 if [[ $delete_old ]]; then
-  sudo nix-collect-garbage --delete-older-than 30d
+  # for system packages
+  sudo nix-collect-garbage --delete-older-than $TIMESPAN
+  # for home-manager
+  nix-collect-garbage --delete-older-than $TIMESPAN
 else
   nix-collect-garbage
 fi
