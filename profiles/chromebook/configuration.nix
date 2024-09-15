9f5027d1e428a368bdb1b3efeead2caae2fe6fee
Gabriele <gmercolino2003@gmail.com>
2024-09-15 15:45:02 +0200

    feat: using new hardware module and moved here fix for power button

--- profiles/chromebook/configuration.nix
+++ profiles/chromebook/configuration.nix
@@ -3,16 +3,26 @@
 {
   imports = [
     ../base/configuration.nix
+    
+    ../../system/hardware
 
     # development
     ../../system/virtualization/docker.nix
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  
+  gab.hardware.bluetooth = true;
+  gab.hardware.pipewire = true;
 
   # I'm not building hyprland on a Pentium 💀
   nix.settings = {
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
+
+  services.logind.extraConfig = ''
+    # don’t shutdown when power button is short-pressed
+    HandlePowerKey=ignore
+  '';
 }
