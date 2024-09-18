39740f04ef810581aacd26bda2e19be363ad4514
Gabriele <gmercolino2003@gmail.com>
2024-09-18 19:59:54 +0200

    feat: added module

---
+++ system/apps/default.nix
@@ -0,0 +1,64 @@
+{ config, lib, pkgs, userSettings, ... }:
+
+let
+  cfg = config.gab.apps;
+in
+{
+  options.gab.apps = {
+    control = lib.mkOption {
+      type = with lib.types; submodule {
+        options = {
+          corectrl = lib.mkEnableOption "corectrl";
+          lact = lib.mkEnableOption "lact";
+        };
+      };
+
+      default = {
+        corectrl = false;
+        lact = false;
+      };
+    }; 
+
+    dev = lib.mkOption {
+      type = with lib.types; submodule {
+        options = {
+          direnv = lib.mkEnableOption "nix-direnv";
+          docker = lib.mkEnableOption "docker";
+        };
+      };
+
+      default = {
+        direnv = false;
+        docker = false;
+      };
+    };
+  };
+
+  config = {
+    users.users.${userSettings.userName}.extraGroups = lib.optionals cfg.control.corectrl [ "corectrl" ]
+                                                       ++ lib.optionals cfg.dev.docker [ "docker" ];
+
+    environment.systemPackages = lib.optionals cfg.control.corectrl [ pkgs.lm_sensors ] 
+                                 ++ lib.optionals cfg.control.lact [ pkgs.lact ];
+
+    # Control related stuff
+    programs.corectrl = {
+      enable = cfg.control.corectrl;
+      gpuOverclock.enable = true;
+    };
+
+    ## lact needs its daemon to properly work
+    systemd.services.lact = {
+      enable = cfg.control.lact;
+      description = "AMDGPU Control Daemon";
+      after = ["multi-user.target"];
+      wantedBy = ["multi-user.target"];
+      serviceConfig.ExecStart = "${pkgs.lact}/bin/lact daemon";
+    };
+
+    # dev related stuff
+    programs.direnv.enable = cfg.dev.direnv;
+    virtualisation.docker.enable = cfg.dev.docker;
+
+  };
+}
