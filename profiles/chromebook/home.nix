147aa2660dbb4992ab7096e4f14ca599efbc4a3c
Gabriele <gmercolino2003@gmail.com>
2024-09-23 10:59:18 +0200

    refactor: using new modules for configuring

--- profiles/chromebook/home.nix
+++ profiles/chromebook/home.nix
@@ -1,15 +1,43 @@
-{ ... }:
+{ pkgs, userSettings, ... }:
 
 {
   imports = [
     ../base/home.nix
-
-    # editors
-    ../../user/apps/editors
-
-    # utilities
-    ../../user/apps/utilities
+    ../../gab/home-manager
+    ../../user/commands/gab
   ];
 
-  gab.editors.intellij = true;
+  gab.shell.aliases = {
+    ls = "${pkgs.eza}/bin/eza --icons";
+    ll = "${pkgs.eza}/bin/eza -l --icons";
+    la = "${pkgs.eza}/bin/eza -la --icons";
+  };
+  gab.shell.zsh = true;
+
+  ########################
+  #         APPS         #
+  ########################
+  gab.apps.utilities.rofi-wayland = true;
+  gab.apps.utilities.gimp = true;
+  gab.apps.utilities.yazi = true;
+
+  gab.apps.control.blueman-applet = true;
+
+  gab.apps.browsers.chrome = true;
+
+  gab.apps.socials.telegram = true;
+
+  gab.apps.terminal.kitty  = true;
+
+  gab.apps.dev.git = {
+    enable    = true;
+    userName  = userSettings.name;
+    userEmail = userSettings.email;
+  };
+
+  ########################
+  #        EDITORS       #
+  ########################
+  gab.apps.editors.nvim            = true;
+  gab.apps.editors.idea-community  = true;
 }
