1c36230580b59bfa570c2aecbfc99f397268671f
Gabriele <gmercolino2003@gmail.com>
2024-09-23 10:59:00 +0200

    chore: removed cachix for hyprland as hyprland-nix takes care of that

--- profiles/chromebook/configuration.nix
+++ profiles/chromebook/configuration.nix
@@ -25,12 +25,6 @@
   gab.apps.dev.direnv     = true;
   gab.apps.dev.docker     = true;
 
-  # I'm not building hyprland on a Pentium 💀
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
-
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
     HandlePowerKey=ignore
