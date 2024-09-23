dd00135c71df5dfa261a1dcccbd4b7e45228142b
Gabriele <gmercolino2003@gmail.com>
2024-09-23 10:50:25 +0200

    fix: fixed path

--- gab/nixos/style/default.nix
+++ gab/nixos/style/default.nix
@@ -27,7 +27,7 @@ in
     stylix.enable = true;
     stylix.autoEnable = true;
 
-    stylix.base16Scheme = ../../../themes + "/${cfg.theme}.yaml";
+    stylix.base16Scheme = ../../../themes/${cfg.theme}.yaml;
     stylix.image = cfg.background;
 
     services.displayManager.sddm.theme = lib.mkForce "${ import ./sddm-theme.nix { inherit pkgs; background  = cfg.background;} }";  
