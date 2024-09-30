f66aadab0f88928fe28959a98aff61fe45c4a180
Gabriele <gmercolino2003@gmail.com>
2024-09-30 16:30:49 +0200

    feat: added desktop entry and updated version

--- gab/home-manager/apps/custom-derivations/zen-browser.nix
+++ gab/home-manager/apps/custom-derivations/zen-browser.nix
@@ -1,11 +1,21 @@
-{ appimageTools, fetchurl }:
-
-appimageTools.wrapType2 rec {
-  name = "zen-browser";
-  version = "1.0.1-a.5";
-
+{ appimageTools, fetchurl, specific ? false }:
+let
+  name    = "zen";
+  version = "1.0.1-a.6";
+  
   src = fetchurl {
-    url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen-generic.AppImage";
-    hash = "sha256-j+z3TQCZ8O74RgQHAKTcE/H8c90LvsBgn/6fvyjKoFU=";
+    url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen-${if specific then "specific" else "generic"}.AppImage";
+    hash = "sha256-1Rho9F0gJUoTWcn5OTVfAJtgeyIhUU0n+pBdVP9n3iU=";
   };
+
+  appimageContents = appimageTools.extract { inherit name version src; };
+in
+appimageTools.wrapType2 {
+  inherit name version src;
+  
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/zen.desktop $out/share/applications/zen.desktop
+    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/128x128/apps/zen.png \
+      $out/share/icons/hicolor/128x128/apps/zen.png
+  '';
 }
