eccaca944f0dd51e823bbc9c29f6e8a3ba228045
Gabriele <gmercolino2003@gmail.com>
2024-10-31 22:36:46 +0100

    chore: bumped up version

--- gab/home-manager/apps/custom-derivations/zen-browser.nix
+++ gab/home-manager/apps/custom-derivations/zen-browser.nix
@@ -1,12 +1,16 @@
-{ appimageTools, fetchurl, specific ? false }:
+{
+  appimageTools,
+  fetchurl,
+  specific ? false,
+}:
 let
-  name    = "zen";
-  version = "1.0.1-a.12";
+  name = "zen";
+  version = "1.0.1-a.16";
 
-  kind = if specific then "specific" else "generic"; 
-  specificHash = "sha256-iBKbOgLQp1+gB80uOk53jlxqtygf/P6x2BfmDghHDPQ=";
-  genericHash  = "sha256-Y43816E558lr/fCNVvCyRhDPPeYUiCKNgaMN4IRgRMo=";
-  
+  kind = if specific then "specific" else "generic";
+  specificHash = "sha256-3+GLBryfIxkDz8tNNZpAnI+XiwJmVsaqo/rSjbWn1OQ=";
+  genericHash = "sha256-uTc2R/VLXzrFjYK2F0G7m0+Ma3SulJRqhIgIcL2VjZk=";
+
   src = fetchurl {
     url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen-${kind}.AppImage";
     hash = if specific then specificHash else genericHash;
@@ -16,7 +20,7 @@ let
 in
 appimageTools.wrapType2 {
   inherit name version src;
-  
+
   extraInstallCommands = ''
     install -m 444 -D ${appimageContents}/zen.desktop $out/share/applications/zen.desktop
     install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/128x128/apps/zen.png \
