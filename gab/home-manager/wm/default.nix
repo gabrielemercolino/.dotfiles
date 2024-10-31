33f04e8d2e7af3e12c18f080f07a0a473d7fa2fb
Gabriele <gmercolino2003@gmail.com>
2024-10-31 23:19:21 +0100

    feat: added option to configure multiple monitors and formatted file

--- gab/home-manager/wm/default.nix
+++ gab/home-manager/wm/default.nix
@@ -1,4 +1,10 @@
-{ config, lib, inputs, pkgs, ... }:
+{
+  config,
+  lib,
+  inputs,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.gab.wm;
@@ -7,22 +13,33 @@ in
   imports = [ inputs.hyprland-nix.homeManagerModules.default ];
 
   options.gab.wm = {
-    hyprland.enable = lib.mkEnableOption "hyprland";
-    bspwm.enable    = lib.mkEnableOption "bspwm";
+    hyprland = {
+      enable = lib.mkEnableOption "hyprland";
+      monitors = lib.mkOption {
+        type = with lib.types; listOf str;
+        default = [ ];
+      };
+    };
+    bspwm.enable = lib.mkEnableOption "bspwm";
   };
 
   config = {
     wayland.windowManager.hyprland = {
-      enable  = cfg.hyprland.enable;
+      enable = cfg.hyprland.enable;
       package = pkgs.hyprland;
 
-      reloadConfig           = true;
-      systemdIntegration     = true;
+      reloadConfig = true;
+      systemdIntegration = true;
       recommendedEnvironment = true;
-      xwayland.enable        = true;
+      xwayland.enable = true;
+
+      config = {
+        monitor = cfg.hyprland.monitors ++ [ ", preferred, auto, 1" ]; # add the default config
+      };
     };
 
     xsession.windowManager.bspwm.enable = cfg.bspwm.enable;
+
   };
-  
+
 }
