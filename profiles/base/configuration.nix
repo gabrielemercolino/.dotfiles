a5a5c9c6c6342ccdcd5e0dcbcf2eac5f10019936
Gabriele <gmercolino2003@gmail.com>
2024-11-03 22:43:21 +0100

    feat: added needed saylock pam setting and formatted file

--- profiles/base/configuration.nix
+++ profiles/base/configuration.nix
@@ -3,22 +3,24 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${userSettings.userName} = {
     isNormalUser = true;
     description = userSettings.name;
     extraGroups = [ "wheel" ];
-    packages = [];
+    packages = [ ];
   };
 
   services.getty.autologinUser = lib.mkDefault userSettings.userName;
+  # to enable swaylock with any compositor other than sway this is needed
+  security.pam.services.swaylock = lib.mkDefault { };
 
   # Allow unfree packages
   nixpkgs.config = {
     allowUnfree = true;
-    allowUnfreePredicate = (_ : true);
-    permittedInsecurePackages = [];
+    allowUnfreePredicate = (_: true);
+    permittedInsecurePackages = [ ];
   };
 
   # whether using x11 or wayland in the end it's better to have it
@@ -26,8 +28,11 @@
 
   system.stateVersion = "23.11";
 
-  nix.settings.experimental-features = ["nix-command" "flakes" ];
-  
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # optimise after every rebuild (not gc) 
   nix.settings.auto-optimise-store = true;
 }
