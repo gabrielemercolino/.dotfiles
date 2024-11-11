530f0c99acaee97a9b267068d30d57b36ba89483
Gabriele <gmercolino2003@gmail.com>
2024-11-11 17:36:20 +0100

    feat: formatted and added hyprpanel

--- flake.nix
+++ flake.nix
@@ -1,57 +1,83 @@
 {
   description = "My flake";
-  
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    
-    systemSettings = {
-      hostName = "nixos";
 
-      dotfiles = "~/.dotfiles";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
 
-      kb = {
-        layout = "it";
-        variant = "";
+      systemSettings = {
+        hostName = "nixos";
+
+        dotfiles = "~/.dotfiles";
+
+        kb = {
+          layout = "it";
+          variant = "";
+        };
+      };
+
+      userSettings = {
+        userName = "gabriele";
+        name = "Gabriele";
+        email = "gmercolino2003@gmail.com";
+
+        wm = "hyprland";
+      };
+
+      lib = nixpkgs.lib;
+
+      createNixosProfile =
+        name: system:
+        lib.nixosSystem {
+          inherit system;
+          modules = [ ./profiles/${name}/configuration.nix ];
+          specialArgs = {
+            inherit
+              userSettings
+              systemSettings
+              inputs
+              outputs
+              ;
+          };
+        };
+
+      createHomeProfile =
+        name: system:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.hyprpanel.overlay
+            ];
+          };
+          modules = [ ./profiles/${name}/home.nix ];
+          extraSpecialArgs = {
+            inherit
+              userSettings
+              systemSettings
+              inputs
+              outputs
+              ;
+          };
+        };
+    in
+    {
+      nixosConfigurations = {
+        mini-pc = createNixosProfile "mini-pc" "x86_64-linux";
+        chromebook = createNixosProfile "chromebook" "x86_64-linux";
+      };
+
+      homeConfigurations = {
+        mini-pc = createHomeProfile "mini-pc" "x86_64-linux";
+        chromebook = createHomeProfile "chromebook" "x86_64-linux";
       };
     };
-
-    userSettings = {
-      userName = "gabriele";
-      name = "Gabriele";
-      email = "gmercolino2003@gmail.com";
-
-      wm = "hyprland";
-    };
-
-    lib = nixpkgs.lib;
-
-    createNixosProfile = name: system: lib.nixosSystem {
-      inherit system;
-      modules = [ ./profiles/${name}/configuration.nix ];
-      specialArgs = { inherit userSettings systemSettings inputs outputs;};
-    };
-
-    createHomeProfile = name: system: home-manager.lib.homeManagerConfiguration { 
-      pkgs = import nixpkgs { inherit system; };
-      modules = [ ./profiles/${name}/home.nix ];
-      extraSpecialArgs = { inherit userSettings systemSettings inputs outputs; };
-    };
-  in {
-    nixosConfigurations = {
-      mini-pc     = createNixosProfile "mini-pc"     "x86_64-linux";
-      chromebook  = createNixosProfile "chromebook"  "x86_64-linux";
-    };
-
-    homeConfigurations = {
-      mini-pc     = createHomeProfile "mini-pc"     "x86_64-linux";
-      chromebook  = createHomeProfile "chromebook"  "x86_64-linux";
-    };
-  };
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
@@ -60,13 +86,14 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-   
+
     nixvim.url = "github:gabrielemercolino/.nixvim";
-    
+
     stylix.url = "github:danth/stylix";
-    
+
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
 
     hyprland-nix.url = "github:hyprland-community/hyprland-nix";
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
   };
 }
