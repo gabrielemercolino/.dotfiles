483f8963a8565fd88891b7a0f5f5dcaaa9b7221f
Gabriele <gmercolino2003@gmail.com>
2024-11-11 15:41:20 +0100

    refactor: indented code

--- gab/nixos/apps/default.nix
+++ gab/nixos/apps/default.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, userSettings, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  userSettings,
+  ...
+}:
 
 let
   cfg = config.gab.apps;
@@ -6,23 +12,21 @@ in
 {
   options.gab.apps = {
     corectrl.enable = lib.mkEnableOption "corectrl";
-    lact.enable     = lib.mkEnableOption "lact";  
-
+    lact.enable = lib.mkEnableOption "lact";
     direnv.enable = lib.mkEnableOption "direnv";
     docker.enable = lib.mkEnableOption "docker";
-
-    ssh.enable  = lib.mkEnableOption "ssh";
+    ssh.enable = lib.mkEnableOption "ssh";
     dbus.enable = lib.mkEnableOption "dbus";
   };
 
   config = {
-    users.users.${userSettings.userName}.extraGroups = lib.optionals cfg.corectrl.enable [ "corectrl" ]
-                                                       ++ 
-                                                       lib.optionals cfg.docker.enable   [ "docker" ];
+    users.users.${userSettings.userName}.extraGroups =
+      lib.optionals cfg.corectrl.enable [ "corectrl" ]
+      ++ lib.optionals cfg.docker.enable [ "docker" ];
 
-    environment.systemPackages = lib.optionals cfg.corectrl.enable [ pkgs.lm_sensors ] 
-                                 ++ 
-                                 lib.optionals cfg.lact.enable     [ pkgs.lact ];
+    environment.systemPackages =
+      lib.optionals cfg.corectrl.enable [ pkgs.lm_sensors ]
+      ++ lib.optionals cfg.lact.enable [ pkgs.lact ];
 
     # Control related stuff
     programs.corectrl = {
@@ -32,15 +36,15 @@ in
 
     ## lact needs its daemon to properly work
     systemd.services.lact = {
-      enable      = cfg.lact.enable;
+      enable = cfg.lact.enable;
       description = "AMDGPU Control Daemon";
-      after       = ["multi-user.target"];
-      wantedBy    = ["multi-user.target"];
+      after = [ "multi-user.target" ];
+      wantedBy = [ "multi-user.target" ];
       serviceConfig.ExecStart = "${pkgs.lact}/bin/lact daemon";
     };
 
     # dev related stuff
-    programs.direnv.enable       = cfg.direnv.enable;
+    programs.direnv.enable = cfg.direnv.enable;
     virtualisation.docker.enable = cfg.docker.enable;
 
     # security related stuff
@@ -48,8 +52,8 @@ in
 
     # services related stuff
     services.dbus = {
-      enable    = cfg.dbus.enable;
-      packages  = [ pkgs.dconf ];
+      enable = cfg.dbus.enable;
+      packages = [ pkgs.dconf ];
     };
     programs.dconf.enable = cfg.dbus.enable;
   };
