494905cf3ca58314b83cfeeb5258be30082f1145
Gabriele <gmercolino2003@gmail.com>
2024-11-18 11:16:17 +0100

    chore: changed home-manager version to 24.11

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
     nixpkgs.url = "nixpkgs/release-24.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
