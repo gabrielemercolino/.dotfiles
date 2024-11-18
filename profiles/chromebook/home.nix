92402e893bc426e5249fba36076270a068f557a2
Gabriele <gmercolino2003@gmail.com>
2024-11-18 13:24:49 +0100

    revert: removed nar thing

--- profiles/chromebook/home.nix
+++ profiles/chromebook/home.nix
@@ -1,29 +1,16 @@
-{
-  pkgs,
-  userSettings,
-  lib,
-  ...
-}:
+{ pkgs, userSettings, ... }:
 
-let
-  hyprpanelNar = ../../nar-cache/hyprpanel.nar;
-  hyprpanelStorePath = "/nix/store/g3xsrgwsfnyc1x8xk9cqpn8f1cq4mk55-hyprpanel";
-in
 {
-  home.activation.importNar = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
-    if ! nix store verify-path ${hyprpanelStorePath}; then
-        echo "Importing ${hyprpanelNar} into the Nix store..."
-        nix store import < ${hyprpanelNar}
-    else
-        echo "Package ${hyprpanelStorePath} already exists in the store."
-    fi
-  '';
-
   imports = [
     ../base/home.nix
     ../../gab/home-manager
     ../../user/commands/gab
   ];
+
+  gab.wm.hyprland = {
+    enable = true;
+    monitors = [ "eDP-1, 1920x1080@60, auto, 1.25" ];
+  };
 
   gab.shell.aliases = {
     ls = "${pkgs.eza}/bin/eza --icons";
@@ -60,10 +47,5 @@ in
     enable = true;
     userName = userSettings.name;
     userEmail = userSettings.email;
-  };
-
-  gab.wm.hyprland = {
-    enable = true;
-    monitors = [ "eDP-1, 1920x1080@60, auto, 1.25" ];
   };
 }
