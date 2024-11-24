4393dc192db261bd8698bfed2064344946d8c824
Gabriele <gmercolino2003@gmail.com>
2024-11-24 15:28:53 +0100

    feat: added vscode

--- gab/home-manager/apps/editors.nix
+++ gab/home-manager/apps/editors.nix
@@ -14,6 +14,16 @@ in
     idea-community.enable = lib.mkEnableOption "idea community edition";
     zed-editor.enable = lib.mkEnableOption "zed editor";
     nvim.enable = lib.mkEnableOption "neovim (with nixvim)";
+    vscode = {
+      enable = lib.mkEnableOption "vsocode";
+      extensions = lib.mkOption {
+        type = with lib.types; listOf package;
+        default = [ ];
+        example = with pkgs.vscode-extensions; [
+          bbenoist.nix
+        ];
+      };
+    };
   };
 
   config = {
@@ -21,5 +31,19 @@ in
       lib.optionals cfg.nvim.enable [ inputs.nixvim.packages.${pkgs.system}.default ]
       ++ lib.optionals cfg.idea-community.enable [ pkgs.jetbrains.idea-community-bin ] # bin = latest 🙄
       ++ lib.optionals cfg.zed-editor.enable [ pkgs.zed-editor ];
+
+    programs.vscode = {
+      enable = true;
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
+      mutableExtensionsDir = false;
+      userSettings = {
+        "editor.fontSize" = 18;
+        "editor.tabSize" = 2;
+        "extensions.autoUpdate" = false;
+      };
+
+      extensions = cfg.vscode.extensions;
+    };
   };
 }
