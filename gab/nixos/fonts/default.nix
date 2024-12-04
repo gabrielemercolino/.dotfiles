bc89bb067eaeed0ba6bb8496d3efcce5b11b8db5
Gabriele <gmercolino2003@gmail.com>
2024-12-04 09:30:53 +0100

    feat: formatted and added font hinting

--- gab/nixos/fonts/default.nix
+++ gab/nixos/fonts/default.nix
@@ -1,10 +1,25 @@
 { pkgs, ... }:
 
 {
-	fonts.packages = with pkgs; [
-		(nerdfonts.override { fonts = [ "Inconsolata" "DejaVuSansMono" "JetBrainsMono" "Meslo" ]; })
-    font-awesome
-    #powerline
-    material-design-icons
-	];
+  fonts = {
+    packages = with pkgs; [
+      (nerdfonts.override {
+        fonts = [
+          "Inconsolata"
+          "DejaVuSansMono"
+          "JetBrainsMono"
+          "Meslo"
+        ];
+      })
+      font-awesome
+      material-design-icons
+    ];
+
+    fontconfig = {
+      antialias = true;
+      hinting = {
+        style = "full";
+      };
+    };
+  };
 }
