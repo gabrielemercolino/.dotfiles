e52fa304b6fb0aa0a8b2ccaa3a496decc56b0ab5
Gabriele <gmercolino2003@gmail.com>
2024-12-04 09:30:20 +0100

    removal: removed vscode

--- gab/home-manager/apps/editors.nix
+++ gab/home-manager/apps/editors.nix
@@ -14,16 +14,6 @@ in
     idea-community.enable = lib.mkEnableOption "idea community edition";
     zed-editor.enable = lib.mkEnableOption "zed editor";
     nvim.enable = lib.mkEnableOption "neovim (with nixvim)";
-    vscode = {
-      enable = lib.mkEnableOption "vsocode";
-      extensions = lib.mkOption {
-        type = with lib.types; listOf package;
-        default = [ ];
-        example = with pkgs.vscode-extensions; [
-          bbenoist.nix
-        ];
-      };
-    };
   };
 
   config = {
@@ -31,19 +21,5 @@ in
       lib.optionals cfg.nvim.enable [ inputs.nixvim.packages.${pkgs.system}.default ]
       ++ lib.optionals cfg.idea-community.enable [ pkgs.jetbrains.idea-community-bin ] # bin = latest 🙄
       ++ lib.optionals cfg.zed-editor.enable [ pkgs.zed-editor ];
-
-    programs.vscode = {
-      enable = true;
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
-      mutableExtensionsDir = false;
-      userSettings = {
-        "editor.fontSize" = 18;
-        "editor.tabSize" = 2;
-        "extensions.autoUpdate" = false;
-      };
-
-      extensions = cfg.vscode.extensions;
-    };
   };
 }
