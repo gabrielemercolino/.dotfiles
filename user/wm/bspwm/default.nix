f6b6cdd06b7fc930dc4244efeb6ad9398d3e714a
Gabriele <gmercolino2003@gmail.com>
2024-12-07 10:48:54 +0100

    refactor: formatted

--- user/wm/bspwm/default.nix
+++ user/wm/bspwm/default.nix
@@ -4,13 +4,15 @@
   services.sxhkd = {
     enable = config.xsession.windowManager.bspwm.enable;
     keybindings = {
-      "super + Return"    = "${pkgs.kitty}/bin/kitty";
-      "super + q"         = "bspc node -k";
-      "super + m"         = "bspc quit";
+      "super + Return" = "${pkgs.kitty}/bin/kitty";
+      "super + q" = "bspc node -k";
+      "super + m" = "bspc quit";
       "super + shift + m" = "bspc wm -r && pkill -USR1 -x sxhkd";
-      
+
+      "super + shift + {p,r}" = "systemctl {poweroff, reboot}";
+
       # apps
-      "super + space"     = "${pkgs.rofi}/bin/rofi -show drun";
+      "super + space" = "${pkgs.rofi}/bin/rofi -show drun";
       "super + shift + h" = "${pkgs.kitty}/bin/kitty ${pkgs.btop}/bin/btop";
 
       # navigation
@@ -28,21 +30,17 @@
     enable = config.xsession.windowManager.bspwm.enable;
     vSync = true;
     settings = {
-      experimental-backends = true; 
+      experimental-backends = true;
     };
     backend = "xr_glx_hybrid";
   };
 
   services.polybar = {
     enable = config.xsession.windowManager.bspwm.enable;
-    script = ''
-      pkill polybar
-
-      polybar main 2>&1 | tee -a /tmp/polybar_main.log & disown
-    '';
+    script = '''';
     settings = {
       "bar/main" = {
-        width  = "100%";
+        width = "100%";
         height = "24pt";
         radius = 6;
 
@@ -50,32 +48,34 @@
       };
 
       "module/cpu" = {
-        type     = "internal/cpu";
+        type = "internal/cpu";
         interval = 1;
         warn-percentage = 95;
       };
     };
   };
 
-  xsession.windowManager.bspwm = { 
-    startupPrograms = [ 
-      "pgrep -x sxhkd > /dev/null || sxhkd" 
-      
+  xsession.windowManager.bspwm = {
+    startupPrograms = [
+      "pgrep -x sxhkd > /dev/null || sxhkd"
+
       "bspc monitor -d 1 2 3 4 5"
       "pkill picom && picom -b"
       "${pkgs.nitrogen}/bin/nitrogen --set-auto ${config.stylix.image}"
+
+      "polybar main 2>&1 | tee -a /tmp/polybar_main.log & disown"
     ];
 
     settings = {
       # window information
       border_width = 3;
-      window_gap   = 14;
+      window_gap = 14;
       pointer_follows_monitor = true;
-      focus_follows_pointer   = true;
-      borderless_monocle      = true;
-      gapless_monocle         = true;
+      focus_follows_pointer = true;
+      borderless_monocle = true;
+      gapless_monocle = true;
       #focused_border_color = "#302D41";
       #normal_border_color = "#1e1e28";
     };
-  }; 
+  };
 }
