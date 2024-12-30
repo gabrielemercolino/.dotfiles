36e5a790316908bb31ae78c8d98593d0590164a6
Gabriele <gmercolino2003@gmail.com>
2024-12-30 18:36:39 +0100

    feat: added wrapper so scripts deps can be installed without cluttering the system

--- user/wm/bspwm/eww/default.nix
+++ user/wm/bspwm/eww/default.nix
@@ -1,13 +1,24 @@
 { pkgs, ... }:
-{
-  home.packages = with pkgs; [
+
+let
+  deps = with pkgs; [
     brightnessctl
     pamixer
     wmctrl
   ];
 
+  eww-wrapper = pkgs.writeShellApplication {
+    name = "eww";
+    runtimeInputs = deps;
+    text = ''
+      exec ${pkgs.eww}/bin/eww "$@" 
+    '';
+  };
+in
+{
   programs.eww = {
     enable = true;
+    package = eww-wrapper;
     configDir = ./config;
   };
 }
