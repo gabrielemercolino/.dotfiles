6d587391aee1d19d39f20167607e40ac0ffc64d1
Gabriele <gmercolino2003@gmail.com>
2025-01-25 11:20:12 +0100

    chore: formatted

--- gab/nixos/gaming/default.nix
+++ gab/nixos/gaming/default.nix
@@ -1,26 +1,28 @@
-{ config, lib, pkgs, userSettings, ... }:
-
-let
-  cfg = config.gab.gaming;
-in
 {
-
+  config,
+  lib,
+  pkgs,
+  userSettings,
+  ...
+}: let
+  cfg = config.gab.gaming;
+in {
   options.gab.gaming = {
-    steam.enable     = lib.mkEnableOption "steam"; 
-    gamemode.enable  = lib.mkEnableOption "gamemode";
+    steam.enable = lib.mkEnableOption "steam";
+    gamemode.enable = lib.mkEnableOption "gamemode";
     gamescope.enable = lib.mkEnableOption "gamescope";
-    suyu.enable      = lib.mkEnableOption "suyu";
+    suyu.enable = lib.mkEnableOption "suyu";
   };
 
   config = {
-    environment.systemPackages = lib.optionals cfg.suyu.enable [ (pkgs.callPackage ./custom-derivations/suyu.nix {} ) ];
+    environment.systemPackages = lib.optionals cfg.suyu.enable [(pkgs.callPackage ./custom-derivations/suyu.nix {})];
 
-    # needed to make the renice setting work 
-    users.users.${userSettings.userName}.extraGroups = lib.optionals cfg.gamemode.enable [ "gamemode" ];
+    # needed to make the renice setting work
+    users.users.${userSettings.userName}.extraGroups = lib.optionals cfg.gamemode.enable ["gamemode"];
 
     programs.steam = {
       enable = cfg.steam.enable;
-      extraCompatPackages = [ pkgs.proton-ge-bin ];
+      extraCompatPackages = [pkgs.proton-ge-bin];
     };
 
     programs.gamemode = {
@@ -33,23 +35,22 @@ in
         };
       };
     };
-    
+
     programs.gamescope = {
       enable = cfg.gamescope.enable;
       env = {
         "XKB_DEFAULT_LAYOUT" = config.services.xserver.xkb.layout; # IMPORTANT: gamescope uses american keyboard layout by default
 
-        "-W" = "1980";  # window width
-        "-H" = "1080";  # window height
+        "-W" = "1980"; # window width
+        "-H" = "1080"; # window height
         #"-r" = "60";    # max refresh rate
       };
       args = [
-        "--mangoapp"  # mango hud (mainly for test)
-        "-f"          # start at full screen
+        "--mangoapp" # mango hud (mainly for test)
+        "-f" # start at full screen
         #"-e"          # enable steam integration
         "--force-windows-fullscreen" # force internal game in full screen
       ];
-
     };
   };
 }
