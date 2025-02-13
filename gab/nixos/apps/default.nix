df1f394a1ca5c08f125193848be221f69d8c86a1
Gabriele <gmercolino2003@gmail.com>
2025-02-13 10:34:07 +0100

    feat: added bashmount

--- gab/nixos/apps/default.nix
+++ gab/nixos/apps/default.nix
@@ -4,12 +4,9 @@
   pkgs,
   userSettings,
   ...
-}:
-
-let
+}: let
   cfg = config.gab.apps;
-in
-{
+in {
   options.gab.apps = {
     corectrl.enable = lib.mkEnableOption "corectrl";
     lact.enable = lib.mkEnableOption "lact";
@@ -17,16 +14,18 @@ in
     docker.enable = lib.mkEnableOption "docker";
     ssh.enable = lib.mkEnableOption "ssh";
     dbus.enable = lib.mkEnableOption "dbus";
+    bashmount.enable = lib.mkEnableOption "bashmount (with udisk2)";
   };
 
   config = {
     users.users.${userSettings.userName}.extraGroups =
-      lib.optionals cfg.corectrl.enable [ "corectrl" ]
-      ++ lib.optionals cfg.docker.enable [ "docker" ];
+      lib.optionals cfg.corectrl.enable ["corectrl"]
+      ++ lib.optionals cfg.docker.enable ["docker"];
 
     environment.systemPackages =
-      lib.optionals cfg.corectrl.enable [ pkgs.lm_sensors ]
-      ++ lib.optionals cfg.lact.enable [ pkgs.lact ];
+      lib.optionals cfg.corectrl.enable [pkgs.lm_sensors]
+      ++ lib.optionals cfg.lact.enable [pkgs.lact]
+      ++ lib.optionals cfg.bashmount.enable [pkgs.bashmount];
 
     # Control related stuff
     programs.corectrl = {
@@ -38,8 +37,8 @@ in
     systemd.services.lact = {
       enable = cfg.lact.enable;
       description = "AMDGPU Control Daemon";
-      after = [ "multi-user.target" ];
-      wantedBy = [ "multi-user.target" ];
+      after = ["multi-user.target"];
+      wantedBy = ["multi-user.target"];
       serviceConfig.ExecStart = "${pkgs.lact}/bin/lact daemon";
     };
 
@@ -53,8 +52,11 @@ in
     # services related stuff
     services.dbus = {
       enable = cfg.dbus.enable;
-      packages = [ pkgs.dconf ];
+      packages = [pkgs.dconf];
     };
     programs.dconf.enable = cfg.dbus.enable;
+
+    # needed for bashmount
+    services.udisks2.enable = cfg.bashmount.enable;
   };
 }
