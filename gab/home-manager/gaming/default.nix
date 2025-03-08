61e24feced8a823cdd7b0a465f5783e991e5f7db
Gabriele <gmercolino2003@gmail.com>
2025-03-08 09:44:21 +0100

    feat: added geforce-now

--- gab/home-manager/gaming/default.nix
+++ gab/home-manager/gaming/default.nix
@@ -1,5 +1,20 @@
-_:{
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.gab.gaming;
+in {
   imports = [
     ./mangohud.nix
   ];
+
+  options.gab.gaming = {
+    geforce.enable = lib.mkEnableOption "geforce-now";
+  };
+
+  config = {
+    home.packages = lib.optionals cfg.geforce.enable [pkgs.gfn-electron];
+  };
 }
