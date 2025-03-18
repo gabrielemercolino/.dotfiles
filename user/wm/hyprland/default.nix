53076510f64a7fbf3774923c00b1e035d9d98f05
Gabriele <gmercolino2003@gmail.com>
2025-03-18 17:04:14 +0100

    chore: formatted

--- user/wm/hyprland/default.nix
+++ user/wm/hyprland/default.nix
@@ -4,9 +4,7 @@
   pkgs,
   systemSettings,
   ...
-}:
-
-{
+}: {
   imports = [
     ./waybar.nix
   ];
@@ -17,78 +15,77 @@
     wl-clipboard
   ];
 
-  wayland.windowManager.hyprland.keyBinds =
-    let
-      MOUSE_L = "mouse:272";
-      MOUSE_R = "mouse:273";
-      # collection of keybinds grouped by functionality
-      groups = {
-        launchApps = {
-          bind."SUPER, RETURN" = "exec, ${pkgs.kitty}/bin/kitty";
-          bind."SUPER, T" = "exec, ${pkgs.telegram-desktop}/bin/telegram-desktop";
-          bind."SUPER_SHIFT, H" = "exec, ${pkgs.kitty}/bin/kitty ${pkgs.btop}/bin/btop";
-          bind."SUPER, SPACE" = "exec, ${pkgs.rofi-wayland}/bin/rofi -show drun";
-        };
-        windowToggles = {
-          bind."SUPER, V" = "togglefloating";
-          bind."SUPER, F" = "fullscreen";
-          bind."SUPER, P" = "pseudo";
-          bind."SUPER, J" = "togglesplit";
-        };
-        mouseWindowControl = {
-          bindm."SUPER, ${MOUSE_L}" = "movewindow";
-          bindm."SUPER, ${MOUSE_R}" = "resizewindow";
-        };
-        audioControl = {
-          binde.", XF86AudioRaiseVolume" = "exec, ${pkgs.pamixer}/bin/pamixer -i 5";
-          binde.", XF86AudioLowerVolume" = "exec, ${pkgs.pamixer}/bin/pamixer -d 5";
-          bind.", XF86AudioMute" = "exec, ${pkgs.pamixer}/bin/pamixer -t";
-        };
-        brightnessControl = {
-          binde.", XF86MonBrightnessUp" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl set +5%";
-          binde.", XF86MonBrightnessDown" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
-        };
-        powerControl = {
-          bind."SUPER_SHIFT, R" = "exec, systemctl reboot";
-          bind."SUPER_SHIFT, P" = "exec, systemctl poweroff";
-        };
-        bar = {
-          bind."SUPER, W" = "exec, pkill waybar; ${pkgs.waybar}/bin/waybar";
-        };
-        moveFocus = {
-          bind."SUPER, left" = "movefocus, l";
-          bind."SUPER, right" = "movefocus, r";
-          bind."SUPER, up" = "movefocus, u";
-          bind."SUPER, down" = "movefocus, d";
-        };
-        changeWorkspace = {
-          bind."SUPER, 0" = "workspace, 0";
-          bind."SUPER, 1" = "workspace, 1";
-          bind."SUPER, 2" = "workspace, 2";
-          bind."SUPER, 3" = "workspace, 3";
-          bind."SUPER, 4" = "workspace, 4";
-          bind."SUPER, 5" = "workspace, 5";
-          bind."SUPER, 6" = "workspace, 6";
-          bind."SUPER, 7" = "workspace, 7";
-          bind."SUPER, 8" = "workspace, 8";
-          bind."SUPER, 9" = "workspace, 9";
-        };
-        moveToWorkspace = {
-          bind."SUPER_SHIFT, 0" = "movetoworkspace, 0";
-          bind."SUPER_SHIFT, 1" = "movetoworkspace, 1";
-          bind."SUPER_SHIFT, 2" = "movetoworkspace, 2";
-          bind."SUPER_SHIFT, 3" = "movetoworkspace, 3";
-          bind."SUPER_SHIFT, 4" = "movetoworkspace, 4";
-          bind."SUPER_SHIFT, 5" = "movetoworkspace, 5";
-          bind."SUPER_SHIFT, 6" = "movetoworkspace, 6";
-          bind."SUPER_SHIFT, 7" = "movetoworkspace, 7";
-          bind."SUPER_SHIFT, 8" = "movetoworkspace, 8";
-          bind."SUPER_SHIFT, 9" = "movetoworkspace, 9";
-        };
+  wayland.windowManager.hyprland.keyBinds = let
+    MOUSE_L = "mouse:272";
+    MOUSE_R = "mouse:273";
+    # collection of keybinds grouped by functionality
+    groups = {
+      launchApps = {
+        bind."SUPER, RETURN" = "exec, ${pkgs.kitty}/bin/kitty";
+        bind."SUPER, T" = "exec, ${pkgs.telegram-desktop}/bin/telegram-desktop";
+        bind."SUPER_SHIFT, H" = "exec, ${pkgs.kitty}/bin/kitty ${pkgs.btop}/bin/btop";
+        bind."SUPER, SPACE" = "exec, ${pkgs.rofi-wayland}/bin/rofi -show drun";
       };
-      screenShot = (pkgs.callPackage ../../commands/screen-shot { });
-      screenRecord = (pkgs.callPackage ../../commands/screen-record { });
-    in
+      windowToggles = {
+        bind."SUPER, V" = "togglefloating";
+        bind."SUPER, F" = "fullscreen";
+        bind."SUPER, P" = "pseudo";
+        bind."SUPER, J" = "togglesplit";
+      };
+      mouseWindowControl = {
+        bindm."SUPER, ${MOUSE_L}" = "movewindow";
+        bindm."SUPER, ${MOUSE_R}" = "resizewindow";
+      };
+      audioControl = {
+        binde.", XF86AudioRaiseVolume" = "exec, ${pkgs.pamixer}/bin/pamixer -i 5";
+        binde.", XF86AudioLowerVolume" = "exec, ${pkgs.pamixer}/bin/pamixer -d 5";
+        bind.", XF86AudioMute" = "exec, ${pkgs.pamixer}/bin/pamixer -t";
+      };
+      brightnessControl = {
+        binde.", XF86MonBrightnessUp" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl set +5%";
+        binde.", XF86MonBrightnessDown" = "exec, ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
+      };
+      powerControl = {
+        bind."SUPER_SHIFT, R" = "exec, systemctl reboot";
+        bind."SUPER_SHIFT, P" = "exec, systemctl poweroff";
+      };
+      bar = {
+        bind."SUPER, W" = "exec, pkill waybar; ${pkgs.waybar}/bin/waybar";
+      };
+      moveFocus = {
+        bind."SUPER, left" = "movefocus, l";
+        bind."SUPER, right" = "movefocus, r";
+        bind."SUPER, up" = "movefocus, u";
+        bind."SUPER, down" = "movefocus, d";
+      };
+      changeWorkspace = {
+        bind."SUPER, 0" = "workspace, 0";
+        bind."SUPER, 1" = "workspace, 1";
+        bind."SUPER, 2" = "workspace, 2";
+        bind."SUPER, 3" = "workspace, 3";
+        bind."SUPER, 4" = "workspace, 4";
+        bind."SUPER, 5" = "workspace, 5";
+        bind."SUPER, 6" = "workspace, 6";
+        bind."SUPER, 7" = "workspace, 7";
+        bind."SUPER, 8" = "workspace, 8";
+        bind."SUPER, 9" = "workspace, 9";
+      };
+      moveToWorkspace = {
+        bind."SUPER_SHIFT, 0" = "movetoworkspace, 0";
+        bind."SUPER_SHIFT, 1" = "movetoworkspace, 1";
+        bind."SUPER_SHIFT, 2" = "movetoworkspace, 2";
+        bind."SUPER_SHIFT, 3" = "movetoworkspace, 3";
+        bind."SUPER_SHIFT, 4" = "movetoworkspace, 4";
+        bind."SUPER_SHIFT, 5" = "movetoworkspace, 5";
+        bind."SUPER_SHIFT, 6" = "movetoworkspace, 6";
+        bind."SUPER_SHIFT, 7" = "movetoworkspace, 7";
+        bind."SUPER_SHIFT, 8" = "movetoworkspace, 8";
+        bind."SUPER_SHIFT, 9" = "movetoworkspace, 9";
+      };
+    };
+    screenShot = pkgs.callPackage ../../commands/screen-shot {};
+    screenRecord = pkgs.callPackage ../../commands/screen-record {};
+  in
     lib.mkMerge [
       groups.powerControl
       groups.launchApps
