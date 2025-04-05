1e25c5d9a24137d05902af6b9c6db081cabdf742
Gabriele <gmercolino2003@gmail.com>
2025-04-05 12:55:09 +0200

    feat: now screen sharing on discord works with wayland

--- gab/home-manager/apps/socials.nix
+++ gab/home-manager/apps/socials.nix
@@ -1,17 +1,19 @@
-{ config, lib, pkgs, ... }:
-
-let
-  cfg = config.gab.apps;
-in
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.gab.apps;
+in {
   options.gab.apps = {
     telegram.enable = lib.mkEnableOption "telegram desktop";
-    discord.enable  = lib.mkEnableOption "discord";
+    discord.enable = lib.mkEnableOption "discord";
   };
-  
+
   config = {
-    home.packages = lib.optionals cfg.telegram.enable [ pkgs.telegram-desktop ]
-                    ++ 
-                    lib.optionals cfg.discord.enable  [ pkgs.discord ];
+    home.packages =
+      lib.optionals cfg.telegram.enable [pkgs.telegram-desktop]
+      ++ lib.optionals cfg.discord.enable [(pkgs.discord.override {withVencord = true;})];
   };
 }
