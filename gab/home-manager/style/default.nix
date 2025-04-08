faade2c2c6d1879f455791c1763e66529fafebe6
Gabriele <gmercolino2003@gmail.com>
2025-04-08 09:48:10 +0200

    fix: formatted and updated hyprland-nix patch for stylix

--- gab/home-manager/style/default.nix
+++ gab/home-manager/style/default.nix
@@ -3,13 +3,10 @@
   lib,
   inputs,
   ...
-}:
-
-let
+}: let
   cfg = config.gab.style;
-in
-{
-  imports = [ inputs.stylix.homeManagerModules.stylix ];
+in {
+  imports = [inputs.stylix.homeManagerModules.stylix];
 
   options.gab.style = {
     background = lib.mkOption {
@@ -29,22 +26,28 @@ in
   };
 
   # fix: even if target is forced to false I need to re-add this otherwise it won't work
-  options.wayland.windowManager.hyprland = {
-    settings = lib.mkOption { default = { }; };
+  options.wayland.windowManager.hyprland.settings = {
+    decoration = lib.mkOption {default = {};};
+    general = lib.mkOption {default = {};};
+    group = lib.mkOption {default = {};};
+    misc = lib.mkOption {default = {};};
   };
 
   config = {
-    stylix.enable = true;
-    stylix.autoEnable = true;
+    stylix = {
+      enable = true;
+      autoEnable = true;
 
-    stylix.base16Scheme = ../../../themes/${cfg.theme}.yaml;
-    stylix.image = cfg.background;
+      base16Scheme = ../../../themes/${cfg.theme}.yaml;
+      image = cfg.background;
+      targets = {
+        mangohud.enable = false;
+        hyprland.enable = lib.mkForce false; # hyprland-nix is not compatible
+        vscode.enable = false;
+      };
+    };
 
-    stylix.targets.mangohud.enable = false;
-    stylix.targets.hyprland.enable = lib.mkForce false; # hyprland-nix is not compatible
-    stylix.targets.vscode.enable = false;
-
-    programs.rofi.theme = lib.mkForce (import ./rofi-theme.nix { inherit config; });
+    programs.rofi.theme = lib.mkForce (import ./rofi-theme.nix {inherit config;});
     programs.swaylock.settings = {
       effect-blur = "7x5";
       effect-vignette = "0.7:0.7";
@@ -52,5 +55,4 @@ in
       clock = true;
     };
   };
-
 }
