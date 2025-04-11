90fa310ee8d0594376217e032b4790f6a82ce16e
Gabriele <gmercolino2003@gmail.com>
2025-04-11 20:35:08 +0200

    feat: formatted and now font sizes are configurable

--- gab/home-manager/style/default.nix
+++ gab/home-manager/style/default.nix
@@ -23,6 +23,25 @@ in {
       ];
       description = "The theme to use";
     };
+
+    fonts.sizes = {
+      applications = lib.mkOption {
+        description = "The font size used for applications";
+        default = 12;
+      };
+      desktop = lib.mkOption {
+        description = "The font size used for window titles, status bars, and other general elements of the desktop";
+        default = 10;
+      };
+      popups = lib.mkOption {
+        description = "The font size used for notifications, popups, and other overlay elements of the desktop";
+        default = 10;
+      };
+      terminal = lib.mkOption {
+        description = "The font size used for terminals and text editors";
+        default = 12;
+      };
+    };
   };
 
   # fix: even if target is forced to false I need to re-add this otherwise it won't work
@@ -37,6 +56,13 @@ in {
     stylix = {
       enable = true;
       autoEnable = true;
+
+      fonts.sizes = {
+        inherit (cfg.fonts.sizes) applications;
+        inherit (cfg.fonts.sizes) desktop;
+        inherit (cfg.fonts.sizes) popups;
+        inherit (cfg.fonts.sizes) terminal;
+      };
 
       base16Scheme = ../../../themes/${cfg.theme}.yaml;
       image = cfg.background;
--- gab/nixos/style/default.nix
+++ gab/nixos/style/default.nix
@@ -4,13 +4,10 @@
   inputs,
   pkgs,
   ...
-}:
-
-let
+}: let
   cfg = config.gab.style;
-in
-{
-  imports = [ inputs.stylix.nixosModules.stylix ];
+in {
+  imports = [inputs.stylix.nixosModules.stylix];
 
   options.gab.style = {
     background = lib.mkOption {
@@ -27,19 +24,46 @@ in
       ];
       description = "The theme to use";
     };
+
+    fonts.sizes = {
+      applications = lib.mkOption {
+        description = "The font size used for applications";
+        default = 12;
+      };
+      desktop = lib.mkOption {
+        description = "The font size used for window titles, status bars, and other general elements of the desktop";
+        default = 10;
+      };
+      popups = lib.mkOption {
+        description = "The font size used for notifications, popups, and other overlay elements of the desktop";
+        default = 10;
+      };
+      terminal = lib.mkOption {
+        description = "The font size used for terminals and text editors";
+        default = 12;
+      };
+    };
   };
 
   config = {
-    stylix.enable = true;
-    stylix.autoEnable = true;
+    stylix = {
+      enable = true;
+      autoEnable = true;
 
-    stylix.base16Scheme = ../../../themes/${cfg.theme}.yaml;
-    stylix.image = cfg.background;
+      base16Scheme = ../../../themes/${cfg.theme}.yaml;
+      image = cfg.background;
+
+      fonts.sizes = {
+        inherit (cfg.fonts.sizes) applications;
+        inherit (cfg.fonts.sizes) desktop;
+        inherit (cfg.fonts.sizes) popups;
+        inherit (cfg.fonts.sizes) terminal;
+      };
+    };
 
     services.displayManager.sddm.theme = lib.mkForce "${import ./sddm-theme.nix {
       inherit pkgs;
-      background = cfg.background;
+      inherit (cfg) background;
     }}";
   };
-
 }
