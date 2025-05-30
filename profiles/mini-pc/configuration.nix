80cb69fbcfed54c49698b1ad2729371c174d9545
Gabriele <gmercolino2003@gmail.com>
2025-05-30 22:26:44 +0200

    chore: refactor

--- profiles/mini-pc/configuration.nix
+++ profiles/mini-pc/configuration.nix
@@ -7,50 +7,55 @@
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  # for amd gpus
-  hardware.amdgpu.initrd.enable = true;
-  hardware.graphics.extraPackages = [pkgs.libva];
+  hardware = {
+    # for amd gpus
+    amdgpu.initrd.enable = true;
+    amdgpu.overdrive.enable = true;
+    graphics.extraPackages = [pkgs.libva];
+  };
   nixpkgs.config.rocmSupport = true;
 
   # use zsh
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
 
-  gab.login.sddm.enable = true;
+  gab = {
+    login.sddm.enable = true;
 
-  gab.hardware = {
-    bluetooth.enable = true;
-    pipewire.enable = true;
-    #pulseaudio.enable = true;
-    amdvlk.enable = false;
+    hardware = {
+      bluetooth.enable = true;
+      pipewire.enable = true;
+      #pulseaudio.enable = true;
+      amdvlk.enable = false;
 
-    i18n.locale = "it_IT.UTF-8";
-    keyboard.layout = "it";
-    time.timeZone = "Europe/Rome";
+      i18n.locale = "it_IT.UTF-8";
+      keyboard.layout = "it";
+      time.timeZone = "Europe/Rome";
+    };
+
+    apps = {
+      ssh.enable = true;
+      dbus.enable = true;
+
+      corectrl.enable = true;
+      lact.enable = true;
+      bashmount.enable = true;
+
+      direnv.enable = true;
+      docker.enable = true;
+    };
+
+    gaming = {
+      steam.enable = true;
+      gamemode.enable = true;
+      gamescope.enable = true;
+      rpcs3.enable = true;
+      suyu.enable = true;
+    };
+
+    wm.hyprland.enable = true;
+    wm.bspwm.enable = true;
   };
-
-  gab.apps = {
-    ssh.enable = true;
-    dbus.enable = true;
-
-    corectrl.enable = true;
-    lact.enable = true;
-    bashmount.enable = true;
-
-    direnv.enable = true;
-    docker.enable = true;
-  };
-
-  gab.gaming = {
-    steam.enable = true;
-    gamemode.enable = true;
-    gamescope.enable = true;
-    rpcs3.enable = true;
-    suyu.enable = true;
-  };
-
-  gab.wm.hyprland.enable = true;
-  gab.wm.bspwm.enable = true;
 
   services.xserver.excludePackages = [pkgs.xterm];
 }
