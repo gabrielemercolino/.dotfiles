3ae39e0b146e72653dfc83bd25e533f04a22d4ae
Gabriele <gmercolino2003@gmail.com>
2025-09-21 22:12:49 +0200

    feat: formatted and added zoxide

--- gab/home-manager/shells/default.nix
+++ gab/home-manager/shells/default.nix
@@ -1,65 +1,81 @@
-{ config, lib, ... }:
-
-let
-  cfg = config.gab.shell;
-in
 {
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.gab.shell;
+in {
   options.gab.shell = {
+    bash.enable = lib.mkEnableOption "bash";
+    zsh.enable = lib.mkEnableOption "zsh";
+    nushell.enable = lib.mkEnableOption "nushell";
+
     aliases = lib.mkOption {
       type = lib.types.attrsOf lib.types.str;
-      example = { l = "ls"; };
+      example = {l = "ls";};
     };
-    bash.enable    = lib.mkEnableOption "bash";
-    zsh.enable     = lib.mkEnableOption "zsh";
-    nushell.enable = lib.mkEnableOption "nushell";
+
+    commands = {
+      z.enable = lib.mkEnableOption "zoxide (smarter cd)";
+    };
   };
 
   config = {
-    programs.zsh = {
-      enable = cfg.zsh.enable;
-      shellAliases = cfg.aliases;
-      oh-my-zsh = {
-        enable = true;
-        theme = "robbyrussell";
-        plugins = ["git" "sudo"];
+    programs = {
+      zsh = {
+        inherit (cfg.zsh) enable;
+        shellAliases = cfg.aliases;
+        oh-my-zsh = {
+          enable = true;
+          theme = "robbyrussell";
+          plugins = ["git" "sudo"];
+        };
+        syntaxHighlighting.enable = true;
+        autosuggestion.enable = true;
       };
-      syntaxHighlighting.enable = true;
-      autosuggestion.enable = true;
-    };
 
-    programs.nushell = {
-      enable = cfg.nushell.enable;
-      shellAliases = cfg.aliases;
-      extraConfig = ''
-        $env.config = {
-          # to hide default banner
-          show_banner: false,
+      nushell = {
+        inherit (cfg.nushell) enable;
+        shellAliases = cfg.aliases;
+        extraConfig = ''
+          $env.config = {
+            # to hide default banner
+            show_banner: false,
 
-          # needed to make direnv work automatically
-          hooks: {
-            pre_prompt: [{ ||
-              if (which direnv | is-empty) {
-                return
-              }
+            # needed to make direnv work automatically
+            hooks: {
+              pre_prompt: [{ ||
+                if (which direnv | is-empty) {
+                  return
+                }
 
-              direnv export json | from json | default {} | load-env
-            }]
+                direnv export json | from json | default {} | load-env
+              }]
+            }
           }
-        }
-      '';
+        '';
+      };
+
+      carapace = {
+        inherit (cfg.nushell) enable;
+        enableNushellIntegration = true;
+      };
+
+      starship.enable = cfg.nushell.enable;
+
+      bash = {
+        inherit (cfg.bash) enable;
+        shellAliases = cfg.aliases;
+      };
+
+      # commands
+      zoxide = {
+        inherit (cfg.commands.z) enable;
+
+        enableBashIntegration = true; # fallback
+        enableZshIntegration = cfg.zsh.enable;
+        enableNushellIntegration = cfg.nushell.enable;
+      };
     };
-
-    programs.carapace = {
-      enable = cfg.nushell.enable;
-      enableNushellIntegration = true;
-    };
-
-    programs.starship.enable = cfg.nushell.enable;
-
-    programs.bash = {
-      enable = cfg.bash.enable;
-      shellAliases = cfg.aliases;
-    };
-
   };
 }
