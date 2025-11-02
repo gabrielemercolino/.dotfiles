919959feb473c7ca0eb17d8c60d4be5b4a47bdc4
Gabriele <gmercolino2003@gmail.com>
2025-11-02 23:12:14 +0100

    refactor: now simpler and works in the same way in x11

--- user/commands/screen-shot/default.nix
+++ user/commands/screen-shot/default.nix
@@ -1,53 +1,39 @@
 {
   writeShellApplication,
-  ffmpeg_6-full,
   grim,
   slurp,
   wl-clipboard,
+  maim,
+  xclip,
 }:
-
 writeShellApplication {
   name = "screen-shot";
 
   runtimeInputs = [
-    ffmpeg_6-full
     grim
     slurp
     wl-clipboard
+    maim
+    xclip
   ];
 
-  text = # bash
+  text =
+    # bash
     ''
-      function is_wayland_session() {
-        if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
-          return 0
-        else
-          return 1
-        fi
-      }
-
-      if [ -z "$XDG_SESSION_TYPE" ]
-      then
-        echo "XDG_SESSION_TYPE is not set, aborting"
+      if [ -z "$XDG_SESSION_TYPE" ]; then
+        echo "XDG_SESSION_TYPE is not set, aborting" >&2
         exit 1
       fi
 
       mkdir -p "$HOME/Pictures/"
-      file_name="$HOME/Pictures/screenshot_$(date +%Y-%m-%d-%T).png"
+      file_name="$HOME/Pictures/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
 
-      if is_wayland_session; then
-        capture_size=$(slurp)
-        
-        grim -g "$capture_size" "$file_name"
-        wl-copy < "$file_name" # also copy to clipboard
+      if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+        slurp | grim -g - "$file_name"
+        wl-copy < "$file_name"
       else
-        #TODO: get size dinamically
-        capture_size=$(xrandr | grep '\*' | awk '{print $1}')
-
-        # shellcheck disable=SC2086
-        ffmpeg -f x11grab -video_size $capture_size -framerate 1 -i :0.0 -vframes 1 $file_name
-
-        #TODO: copy to clipboard if possible
+        maim -s "$file_name"
+        xclip -selection clipboard -t image/png -i "$file_name" 2>/dev/null || true
       fi
     '';
 }
