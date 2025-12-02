489dbf45a8475c94a32fa322d11339224115fdf6
Gabriele <gmercolino2003@gmail.com>
2025-12-02 10:56:02 +0100

    chore: added link for checking cachix for hyprland

--- profiles/base/configuration.nix
+++ profiles/base/configuration.nix
@@ -59,6 +59,7 @@
     # optimise after every rebuild (not gc)
     auto-optimise-store = true;
 
+    # https://wiki.hypr.land/Nix/Cachix/
     substituters = ["https://hyprland.cachix.org"];
     trusted-substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
