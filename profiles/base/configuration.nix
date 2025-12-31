3e90eec03a430c232f3f27bd4098bf59eb6a0c86
Gabriele <gmercolino2003@gmail.com>
2025-12-31 12:31:08 +0100

    feat: added systemd at initrd

--- profiles/base/configuration.nix
+++ profiles/base/configuration.nix
@@ -5,6 +5,8 @@
   pkgs,
   ...
 }: {
+  boot.initrd.systemd.enable = true;
+  boot.initrd.systemd.emergencyAccess = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
