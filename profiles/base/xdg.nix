6c6a62727a936d7c7e60a94cce714fe5d12aa87f
Gabriele <gmercolino2003@gmail.com>
2026-01-12 22:18:45 +0100

    feat: added expansion w/ custom exclusion support & refined config

---
+++ profiles/base/xdg.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  defaultApplications = let
+    allMimeTypes = builtins.readFile "${pkgs.shared-mime-info}/share/mime/types";
+
+    expandMimeGlobExcluding = glob: exclude: let
+      globPrefix = lib.removeSuffix "*" glob;
+      matchesGlob = mime: lib.hasPrefix globPrefix mime;
+      isNotExcluded = mime: !(builtins.elem mime exclude);
+    in
+      allMimeTypes
+      |> lib.splitString "\n"
+      |> builtins.filter matchesGlob
+      |> builtins.filter isNotExcluded;
+
+    browser = "zen-beta.desktop";
+    imageViewer = "imv.desktop";
+    gimp = "gimp.desktop";
+    videoViewer = "mpv.desktop";
+    textEditor = "editor.desktop";
+
+    gimpFormats = ["image/x-xcf" "image/x-psd" "image/x-xcfgz"];
+    browserFormats = ["application/xhtml+xml" "application/pdf" "application/xml"];
+
+    imageFormats = expandMimeGlobExcluding "image/*" gimpFormats;
+    videoFormats = expandMimeGlobExcluding "video/*" [];
+    textFormats = expandMimeGlobExcluding "text/*" [];
+
+    toMimeAttrs = formats: opener:
+      formats
+      |> map (n: {
+        name = n;
+        value = opener;
+      })
+      |> builtins.listToAttrs;
+  in
+    lib.mkMerge [
+      (toMimeAttrs imageFormats imageViewer)
+      (toMimeAttrs gimpFormats gimp)
+      (toMimeAttrs videoFormats videoViewer)
+      (toMimeAttrs textFormats textEditor)
+      (toMimeAttrs browserFormats browser)
+    ];
+
+  desktopEntries = {
+    imv = {
+      name = "imv";
+      exec = "${lib.getExe pkgs.imv}";
+    };
+    mpv = {
+      name = "mpv";
+      exec = "${lib.getExe pkgs.mpv} --keep-open=yes";
+    };
+  };
+}
