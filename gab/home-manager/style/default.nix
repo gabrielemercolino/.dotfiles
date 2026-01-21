b13078b6ed16681a68be647413c6e6aa2ed34948
Gabriele <gmercolino2003@gmail.com>
2026-01-21 18:05:37 +0100

    refactor: moved stylix options in common module

--- gab/home-manager/style/default.nix
+++ gab/home-manager/style/default.nix
@@ -14,38 +14,6 @@
 in {
   imports = [inputs.stylix.homeModules.stylix ./hyprnix.nix];
 
-  options.gab.style = {
-    theme = lib.mkOption {
-      default = "catppuccin-mocha";
-      type = lib.types.enum (
-        builtins.readDir ../../../themes
-        |> builtins.attrNames
-        |> builtins.filter (name: lib.strings.hasSuffix ".nix" name)
-        |> map (lib.removeSuffix ".nix")
-      );
-      description = "The theme to use";
-    };
-
-    fonts.sizes = {
-      applications = lib.mkOption {
-        description = "The font size used for applications";
-        default = 12;
-      };
-      desktop = lib.mkOption {
-        description = "The font size used for window titles, status bars, and other general elements of the desktop";
-        default = 10;
-      };
-      popups = lib.mkOption {
-        description = "The font size used for notifications, popups, and other overlay elements of the desktop";
-        default = 10;
-      };
-      terminal = lib.mkOption {
-        description = "The font size used for terminals and text editors";
-        default = 12;
-      };
-    };
-  };
-
   config = {
     stylix = {
       enable = true;
