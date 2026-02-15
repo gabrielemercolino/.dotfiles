0ac948b4d92dde8bc4848907374bfe4a9b31c07f
Gabriele <gmercolino2003@gmail.com>
2026-02-15 09:49:14 +0100

    refactor: moved silentSDDM style in a separate file

--- gab/nixos/modules/style/default.nix
+++ gab/nixos/modules/style/default.nix
@@ -1,24 +1,14 @@
 {
-  lib,
   config,
   inputs,
-  pkgs,
-  userSettings,
   ...
 }: let
   cfg = config.gab.style;
-  theme = import ../../../../themes/${config.gab.style.theme}.nix {inherit config pkgs lib;};
+  theme = cfg._theme;
   background = theme.background or ./wallpaper.png;
-  profile = theme.profile or null;
-  extras = theme.extras or {};
   fonts = theme.fonts or {};
-
-  imgName = img:
-    if lib.isDerivation img
-    then img.name
-    else builtins.baseNameOf img;
 in {
-  imports = [inputs.stylix.nixosModules.stylix inputs.silentSDDM.nixosModules.default];
+  imports = [inputs.stylix.nixosModules.default];
 
   config = {
     stylix = {
@@ -40,31 +30,5 @@ in {
           };
         };
     };
-
-    programs.silentSDDM = lib.mkMerge [
-      {
-        enable = true;
-        theme = "default";
-        backgrounds = {bg = config.stylix.image;};
-        # TODO: add default so themes without it won't have the last profile pic set
-        profileIcons = lib.mkIf (profile != null) {
-          "${userSettings.userName}" = profile;
-        };
-        settings = {
-          "LoginScreen" = {
-            blur = 32;
-            background = "${imgName config.stylix.image}";
-          };
-          "LoginScreen.MenuArea.Keyboard" = {
-            display = false;
-          };
-          "LockScreen" = {
-            blur = 0;
-            background = "${imgName config.stylix.image}";
-          };
-        };
-      }
-      (extras.silentSDDM or {})
-    ];
   };
 }
---
+++ gab/nixos/modules/style/silentSDDM.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  config,
+  inputs,
+  userSettings,
+  ...
+}: let
+  cfg = config.gab.style;
+  theme = cfg._theme;
+  background = theme.background or ./wallpaper.png;
+  profile = theme.profile or null;
+  extras = theme.extras or {};
+
+  imgName = img:
+    if lib.isDerivation img
+    then img.name
+    else builtins.baseNameOf img;
+in {
+  imports = [inputs.silentSDDM.nixosModules.default];
+
+  config = {
+    programs.silentSDDM = lib.mkMerge [
+      {
+        enable = true;
+        theme = "default";
+        backgrounds = {bg = background;};
+        # TODO: add default so themes without it won't have the last profile pic set
+        profileIcons = lib.mkIf (profile != null) {
+          "${userSettings.userName}" = profile;
+        };
+        settings = {
+          "LoginScreen" = {
+            blur = 32;
+            background = "${imgName background}";
+          };
+          "LoginScreen.MenuArea.Keyboard" = {
+            display = false;
+          };
+          "LockScreen" = {
+            blur = 0;
+            background = "${imgName background}";
+          };
+        };
+      }
+      (extras.silentSDDM or {})
+    ];
+  };
+}
