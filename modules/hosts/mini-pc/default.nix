1086e46f0e2fe6958ba5e9ab48190c81fc02a28e
gabriele <gmercolino2003@gmail.com>
2026-05-01 20:36:14 +0200

    chore: formatted cachyos kernel cache

--- modules/hosts/mini-pc/default.nix
+++ modules/hosts/mini-pc/default.nix
@@ -39,9 +39,11 @@ in
 
         nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
 
-        nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
-        nix.settings.trusted-substituters = [ "https://attic.xuyh0120.win/lantian" ];
-        nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+        nix.settings = rec {
+          substituters = [ "https://attic.xuyh0120.win/lantian" ];
+          trusted-substituters = substituters;
+          trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+        };
 
         # boot.kernelPackages = pkgs.linuxPackages_latest;
         boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-x86_64-v3;
