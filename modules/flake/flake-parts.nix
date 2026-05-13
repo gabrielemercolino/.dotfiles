00c831b2c00629251039a0f799819f2b02146f41
gabriele <gmercolino2003@gmail.com>
2026-05-13 15:51:10 +0200

    feat(modules): added custom implemetation of modules

--- modules/flake/flake-parts.nix
+++ modules/flake/flake-parts.nix
@@ -1,4 +1,33 @@
-{ inputs, ... }:
+{ lib, moduleLocation, ... }:
+
+let
+  inherit (lib)
+    mapAttrs
+    mkOption
+    types
+    ;
+
+  escapeNixIdentifier =
+    s: if builtins.match "[a-zA-Z_][a-zA-Z0-9_'\\-]*" s != null then s else lib.escapeNixString s;
+
+  wrapModule = class: moduleName: module: {
+    key = "${toString moduleLocation}#modules.${escapeNixIdentifier class}.${escapeNixIdentifier moduleName}";
+    _file = "${toString moduleLocation}#modules.${escapeNixIdentifier class}.${escapeNixIdentifier moduleName}";
+    _class = class;
+    imports = [ module ];
+  };
+in
 {
-  imports = [ inputs.flake-parts.flakeModules.modules ];
+  # imports = [ inputs.flake-parts.flakeModules.modules ];
+
+  # Basically the same thing but with the key for each module
+  options.flake.modules = mkOption {
+    type = types.lazyAttrsOf (types.lazyAttrsOf types.deferredModule);
+    default = { };
+    apply = mapAttrs (class: mapAttrs (wrapModule class));
+    description = ''
+      Groups of modules published by the flake, with automatic `key` for deduplication.
+    '';
+  };
+
 }
