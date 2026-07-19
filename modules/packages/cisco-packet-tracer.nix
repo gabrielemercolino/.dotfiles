06a810be94a4506d52b1c7f0c23db33020004770
gabriele <ciruzzo032@noreply.codeberg.org>
2026-07-19 23:21:56 +0200

    chore(cisco-packet-tracer): added desktop entry

--- modules/packages/cisco-packet-tracer.nix
+++ modules/packages/cisco-packet-tracer.nix
@@ -35,9 +35,29 @@
 
         extraBwrapArgs = [ "--setenv QT_QPA_PLATFORM xcb" ];
 
-        extraInstallCommands = ''
-          mv $out/bin/cisco-packet-tracer $out/bin/packettracer9
-        '';
+        extraInstallCommands =
+          let
+            contents = pkgs.appimageTools.extract {
+              pname = "cisco-packet-tracer";
+              inherit version;
+              src = appimage;
+            };
+          in
+          ''
+            mv $out/bin/cisco-packet-tracer $out/bin/packettracer9
+
+            install -Dm444 ${contents}/CiscoPacketTracer-9.0.0.desktop $out/share/applications/cisco-packet-tracer-9.desktop
+            substituteInPlace $out/share/applications/* \
+              --replace-fail "Exec=@EXEC_PATH@" "Exec=packettracer9" \
+              --replace-fail "Icon=app" "Icon=cisco-packet-tracer-9"
+
+            install -Dm444 ${contents}/usr/share/icons/hicolor/48x48/apps/app.png $out/share/icons/hicolor/48x48/apps/cisco-packet-tracer-9.png
+            cp -r ${contents}/usr/share/icons/gnome/48x48/mimetypes $out/share/icons/hicolor/48x48/
+
+            for desktop in $out/share/applications/*.desktop; do
+              sed -i '/^\[Desktop Entry\]/a StartupWMClass=PacketTracer' "$desktop"
+            done
+          '';
 
         meta = {
           mainProgram = "packettracer9";
