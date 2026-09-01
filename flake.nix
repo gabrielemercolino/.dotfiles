54b0dba8064ecc03ee511c101eb39851c0d26f71
gabriele <ciruzzo032@noreply.codeberg.org>
2026-09-01 21:46:43 +0200

    chore: updated lock

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
-    nixpkgs.url = "nixpkgs/174eb786fb68e3a13e4e535a3deea479a0c07a6a";
+    nixpkgs.url = "nixpkgs/e8be7818e19ada32105a8af937a6a473b38167ca";
 
     home-manager = {
       url = "github:nix-community/home-manager";
