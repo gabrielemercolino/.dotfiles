90f7679fa9f24a6ee61947f7642dd5beaa2cc0a0
gabriele <ciruzzo032@noreply.codeberg.org>
2026-09-12 16:41:40 +0200

    feat(home-server): added samba config

--- modules/hosts/home-server/default.nix
+++ modules/hosts/home-server/default.nix
@@ -36,6 +36,7 @@ in
         # ZFS pool config
         boot.supportedFilesystems.zfs = true;
         boot.zfs.package = config.boot.kernelPackages.zfs_cachyos;
+        boot.zfs.extraPools = [ "tank" ];
         networking.hostId = "23b06696";
 
         # has specific optimisations for this pc
@@ -60,6 +61,41 @@ in
 
         services = {
           tailscale.enable = true;
+
+          samba = {
+            enable = true;
+            openFirewall = true;
+
+            settings = {
+              global = {
+                "workgroup" = "WORKGROUP";
+                "server string" = "HomeServer";
+                "netbios name" = "HOMESERVER";
+                "security" = "user";
+                "map to guest" = "never";
+              };
+              share = {
+                path = "/tank/share";
+                browseable = "yes";
+                "read only" = "no";
+                "valid users" = "gabriele";
+                "force user" = "gabriele";
+              };
+            };
+          };
+
+          samba-wsdd = {
+            enable = true;
+            openFirewall = true;
+            discovery = true;
+            interface = "wlp6s0";
+          };
+
+          avahi = {
+            enable = true;
+            publish.enable = true;
+            publish.userServices = true;
+          };
         };
       };
 
