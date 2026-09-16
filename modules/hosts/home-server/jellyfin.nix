28d8c64d3bf4564f19ff252c9b966cb0c1f63c54
gabriele <ciruzzo032@noreply.codeberg.org>
2026-09-16 22:49:00 +0200

    feat(home-server): jellyfin now has reverse proxy

--- modules/hosts/home-server/jellyfin.nix
+++ modules/hosts/home-server/jellyfin.nix
@@ -1,17 +1,44 @@
 { ... }:
 {
   hosts.home-server = {
-    nixos = { user, ... }: {
-      systemd.tmpfiles.rules = [ "d /tank/jellyfin 0755 ${user.name} users -" ];
+    nixos =
+      {
+        config,
+        pkgs,
+        user,
+        ...
+      }:
+      {
+        systemd.tmpfiles.rules = [ "d /tank/jellyfin 0755 ${user.name} users -" ];
 
-      services = {
-        jellyfin = {
-          enable = true;
-          dataDir = "/tank/jellyfin";
-          user = user.name;
-          openFirewall = true;
+        sops.secrets = {
+          "cloudflared/jellyfin" = { };
+        };
+
+        services = {
+          jellyfin = {
+            enable = true;
+            dataDir = "/tank/jellyfin";
+            user = user.name;
+            openFirewall = true;
+          };
+        };
+
+        systemd.services.cloudflared-jellyfin = {
+          description = "Cloudflare Tunnel";
+          after = [
+            "network-online.target"
+            "jellyfin.service"
+          ];
+          wants = [ "network-online.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            DynamicUser = true;
+            LoadCredential = "token:${config.sops.secrets."cloudflared/jellyfin".path}";
+            ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token-file=%d/token";
+            Restart = "on-failure";
+          };
         };
       };
-    };
   };
 }
